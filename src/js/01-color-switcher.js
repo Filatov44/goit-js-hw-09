14585a86ba80ca3445acb5566de239f083a0271e
Filatov44 <filatov300@gmail.com>
2022-07-18 13:53:20 +0300

    add change in js file color-switcher

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,27 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  closeBtn: document.querySelector('[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
+
+refs.startBtn.addEventListener('click', startChangeColor);
+refs.closeBtn.addEventListener('click', stopChangeColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+function BGChangeColor() {
+  return (refs.bodyEl.style.backgroundColor = getRandomHexColor());
+}
+
+function startChangeColor() {
+  refs.startBtn.setAttribute('disabled', 'disabled');
+  refs.closeBtn.removeAttribute('disabled');
+  timerId = setInterval(BGChangeColor, 1000);
+}
+
+function stopChangeColor() {
+  refs.closeBtn.setAttribute('disabled', 'disabled');
+  refs.startBtn.removeAttribute('disabled');
+  clearInterval(timerId);
+}
