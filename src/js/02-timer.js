b67ff476c79c3ec1637f130b3b6a9c6860907b09
Filatov44 <filatov300@gmail.com>
2022-07-22 23:07:48 +0300

    add timer reset-function

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+// import 'flatpickr/dist/themes/dark.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
@@ -10,10 +11,14 @@ const refs = {
   hoursEl: document.querySelector('[data-hours]'),
   minutesEl: document.querySelector('[data-minutes]'),
   secondsEl: document.querySelector('[data-seconds]'),
+  //reset
+  buttonResetEl: document.querySelector('[data-reset]'),
 };
 
 const dataNow = new Date();
-let getTimeUser;
+let getTimeUser = null;
+//reset
+let timeId;
 
 const options = {
   enableTime: true,
@@ -21,13 +26,16 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    // console.log(selectedDates[0]);
     const getSelectDates = selectedDates[0].getTime();
     const getDataNow = dataNow.getTime();
     if (getSelectDates < getDataNow) {
       Notify.failure('Please choose a date in the future');
     } else {
       refs.buttonStartEl.removeAttribute('disabled');
+
+      //color start button
+      refs.buttonStartEl.classList.add('timer-button__start');
+
       Notify.success('Ok! Let go!');
       getTimeUser = getSelectDates;
       refs.buttonStartEl.addEventListener('click', timer);
@@ -37,6 +45,8 @@ const options = {
 
 // Кнопка старт вначале неактивна
 refs.buttonStartEl.setAttribute('disabled', true);
+//Reset
+refs.buttonResetEl.setAttribute('disabled', true);
 
 // Flatpickr запускаем
 
@@ -44,15 +54,28 @@ flatpickr(refs.inputEl, options);
 
 // Функция Таймер
 function timer() {
-  const timeId = setInterval(() => {
+  timeId = setInterval(() => {
     // текущая дата на момент нажатия кнопки старт
     const dateNowClick = new Date();
     const deltaTime = getTimeUser - dateNowClick;
     if (deltaTime > 0) {
       updateInterfaceTimer(convertMs(deltaTime));
       refs.buttonStartEl.setAttribute('disabled', true);
+      //input off
+      refs.inputEl.setAttribute('disabled', true);
+
+      //Снимаем неактивность с ресет
+      refs.buttonResetEl.removeAttribute('disabled');
+      refs.buttonResetEl.classList.add('timer-button__start');
+
+      //color start button
+      refs.buttonStartEl.classList.remove('timer-button__start');
     } else {
       clearInterval(timeId);
+      Notify.info('Time is over');
+      refs.buttonResetEl.classList.remove('timer-button__start');
+      refs.buttonResetEl.setAttribute('disabled', true);
+      refs.inputEl.removeAttribute('disabled');
     }
   }, 1000);
   //   console.log('Привет');
@@ -91,4 +114,16 @@ function convertMs(ms) {
 //Форматирует значение добавляя ноль при необходимости
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
+}
+
+//Функционал RESET
+refs.buttonResetEl.addEventListener('click', onReset);
+
+function onReset() {
+  refs.inputEl.removeAttribute('disabled');
+  refs.buttonResetEl.setAttribute('disabled', true);
+  updateInterfaceTimer(convertMs(0));
+  clearInterval(timeId);
+  Notify.warning('Attention! Timer has been reset');
+  refs.buttonResetEl.classList.remove('timer-button__start');
 }
